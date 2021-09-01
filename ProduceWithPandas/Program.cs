a8f8f326085f27e8a0b25c609b4701699ee609e8
Dong Bin <popmessiah@hotmail.com>
2021-09-02 00:52:36 +0800

    feat: Use ant design

--- ProduceWithPandas/Program.cs
+++ ProduceWithPandas/Program.cs
@@ -5,5 +5,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddAntDesign();
 
 await builder.Build().RunAsync();

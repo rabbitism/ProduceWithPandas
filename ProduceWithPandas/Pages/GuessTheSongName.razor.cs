341ba1bfecb6f91cd7c2b28615766ca4614f6f60
Dong Bin <popmessiah@hotmail.com>
2021-09-22 21:10:30 +0800

    fix: add more usings.

--- ProduceWithPandas/Pages/GuessTheSongName.razor.cs
+++ ProduceWithPandas/Pages/GuessTheSongName.razor.cs
@@ -5,154 +5,161 @@ using ProduceWithPandas.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 using System.Net.Http.Json;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Net.Http;
 
-namespace ProduceWithPandas.Pages;
-
-public partial class GuessTheSongName : ComponentBase
+namespace ProduceWithPandas.Pages
 {
-    private string _actualName;
-    private string _actualNameBytes;
-    private string _testName;
-    private string _testNameBytes;
-    private double _cross;
-    private double _actualMod;
-    private double _testMod;
-    private double _result;
-    private List<DoubanReply> _records;
-    private bool _loading;
-    private DateTime _lastRefreshTime;
-    [Inject] HttpClient Client { get; set; }
-
-    public GuessTheSongName()
+    public partial class GuessTheSongName : ComponentBase
     {
-        _actualName = string.Empty;
-        _actualNameBytes = string.Empty;
-        _testName = string.Empty;
-        _testNameBytes = string.Empty;
-        _records = new List<DoubanReply>();
+        private string _actualName;
+        private string _actualNameBytes;
+        private string _testName;
+        private string _testNameBytes;
+        private double _cross;
+        private double _actualMod;
+        private double _testMod;
+        private double _result;
+        private List<DoubanReply> _records;
+        private bool _loading;
+        private DateTime _lastRefreshTime;
+        [Inject] HttpClient Client { get; set; }
 
-        _actualName = "就算与全世界为敌也要跟你在一起";
-        _actualNameBytes = string.Join(", ", GetBytesFromString(_actualName).Select(a => a.ToString("X2")));
-    }
-
-    protected override Task OnInitializedAsync()
-    {
-
-        return base.OnInitializedAsync();
-    }
-
-    public void OnSearch()
-    {
-        if (_testName?.Length != 15)
+        public GuessTheSongName()
         {
-            return;
+            _actualName = string.Empty;
+            _actualNameBytes = string.Empty;
+            _testName = string.Empty;
+            _testNameBytes = string.Empty;
+            _records = new List<DoubanReply>();
+
+            _actualName = "就算与全世界为敌也要跟你在一起";
+            _actualNameBytes = string.Join(", ", GetBytesFromString(_actualName).Select(a => a.ToString("X2")));
         }
-        byte[] bytes1 = GetBytesFromString(_actualName);
-        byte[] bytes2 = GetBytesFromString(_testName);
-        _actualNameBytes = string.Join(", ", bytes1.Select(a => a.ToString("X2")));
-        _testNameBytes = string.Join(", ", bytes2.Select(a => a.ToString("X2")));
-        _cross = GetCross(bytes1, bytes2);
-        _actualMod = GetMod(bytes1);
-        _testMod = GetMod(bytes2);
-        _result = _cross * 100.0 / _actualMod / _testMod;
-    }
 
-    private byte[] GetBytesFromString(string s)
-    {
-        if (s is null)
+        protected override Task OnInitializedAsync()
         {
-            return Array.Empty<byte>();
+
+            return base.OnInitializedAsync();
         }
-        return Encoding.Unicode.GetBytes(s);
-    }
 
-    private double GetMod(byte[] bytes)
-    {
-        double result = 0;
-        foreach (byte b in bytes)
+        public void OnSearch()
         {
-            result += (1.0 * b * b);
-        }
-        return Math.Sqrt(result);
-    }
-
-    private double GetCross(byte[] bytes1, byte[] bytes2)
-    {
-        double result = 0;
-        int length = Math.Min(bytes1.Length, bytes2.Length);
-        for (int i = 0; i < length; i++)
-        {
-            result += (1.0 * bytes1[i] * bytes2[i]);
-        }
-        return result;
-    }
-
-    private async void OnClick()
-    {
-        var api = @"https://rabbitism.com/api/api/doubanpost/244506252";
-        _loading = true;
-        var results= await Client.GetFromJsonAsync<DoubanPostCache>(api);
-        
-        if (results != null)
-        {
-            _lastRefreshTime = results.LastUpdateDate;
-            _records.Clear();
-            Dictionary<int, List<DoubanReply>> _userMap = new Dictionary<int, List<DoubanReply>>();
-            results.Replies.OrderBy(a => a.PublishTime);
-            foreach(var reply in results.Replies)
+            if (_testName?.Length != 15)
             {
-                if (!_userMap.ContainsKey(reply.UserId))
-                {
-                    _userMap[reply.UserId] = new List<DoubanReply>();
-                }
-                if (_userMap[reply.UserId].Count >= 3)
-                {
-                    continue;
-                }
-                _userMap[reply.UserId].Add(reply);
+                return;
             }
+            byte[] bytes1 = GetBytesFromString(_actualName);
+            byte[] bytes2 = GetBytesFromString(_testName);
+            _actualNameBytes = string.Join(", ", bytes1.Select(a => a.ToString("X2")));
+            _testNameBytes = string.Join(", ", bytes2.Select(a => a.ToString("X2")));
+            _cross = GetCross(bytes1, bytes2);
+            _actualMod = GetMod(bytes1);
+            _testMod = GetMod(bytes2);
+            _result = _cross * 100.0 / _actualMod / _testMod;
+        }
 
-            List<DoubanReply> _replies = new List<DoubanReply>();
-            foreach(var value in _userMap.Values)
+        private byte[] GetBytesFromString(string s)
+        {
+            if (s is null)
             {
-                _replies.AddRange(value);
+                return Array.Empty<byte>();
             }
-            foreach(var record in _replies)
+            return Encoding.Unicode.GetBytes(s);
+        }
+
+        private double GetMod(byte[] bytes)
+        {
+            double result = 0;
+            foreach (byte b in bytes)
             {
-                if(record.PublishTime>=new DateTime(2021, 09, 21))
+                result += (1.0 * b * b);
+            }
+            return Math.Sqrt(result);
+        }
+
+        private double GetCross(byte[] bytes1, byte[] bytes2)
+        {
+            double result = 0;
+            int length = Math.Min(bytes1.Length, bytes2.Length);
+            for (int i = 0; i < length; i++)
+            {
+                result += (1.0 * bytes1[i] * bytes2[i]);
+            }
+            return result;
+        }
+
+        private async void OnClick()
+        {
+            var api = @"https://rabbitism.com/api/api/doubanpost/244506252";
+            _loading = true;
+            var results = await Client.GetFromJsonAsync<DoubanPostCache>(api);
+
+            if (results != null)
+            {
+                _lastRefreshTime = results.LastUpdateDate;
+                _records.Clear();
+                Dictionary<int, List<DoubanReply>> _userMap = new Dictionary<int, List<DoubanReply>>();
+                results.Replies.OrderBy(a => a.PublishTime);
+                foreach (var reply in results.Replies)
                 {
-                    continue;
-                }
-                string? answer = record.Answer?.Trim();
-                if(answer != null)
-                {
-                    string actualAnswer = string.Empty;
-                    if (answer.Length == 15)
+                    if (!_userMap.ContainsKey(reply.UserId))
                     {
-                        actualAnswer = answer;
+                        _userMap[reply.UserId] = new List<DoubanReply>();
                     }
-                    else if (answer.Length==17 && answer.StartsWith("《") && answer.EndsWith("》"))
-                    {
-                        actualAnswer = answer.Substring(1, 15);
-                    }
-                    else if(answer.StartsWith("歌名是：") && answer.Length >= 19)
-                    {
-                        actualAnswer = answer.Substring(4, 15);
-                    }
-                    else
+                    if (_userMap[reply.UserId].Count >= 3)
                     {
                         continue;
                     }
-                    byte[] bytes1 = GetBytesFromString(_actualName);
-                    byte[] bytes2 = GetBytesFromString(actualAnswer);
-                    record.Score = 100.0 * GetCross(bytes1, bytes2) / GetMod(bytes1) / GetMod(bytes2);
-                    record.Answer = actualAnswer;
-                    _records.Add(record);
+                    _userMap[reply.UserId].Add(reply);
                 }
+
+                List<DoubanReply> _replies = new List<DoubanReply>();
+                foreach (var value in _userMap.Values)
+                {
+                    _replies.AddRange(value);
+                }
+                foreach (var record in _replies)
+                {
+                    if (record.PublishTime >= new DateTime(2021, 09, 21))
+                    {
+                        continue;
+                    }
+                    string? answer = record.Answer?.Trim();
+                    if (answer != null)
+                    {
+                        string actualAnswer = string.Empty;
+                        if (answer.Length == 15)
+                        {
+                            actualAnswer = answer;
+                        }
+                        else if (answer.Length == 17 && answer.StartsWith("《") && answer.EndsWith("》"))
+                        {
+                            actualAnswer = answer.Substring(1, 15);
+                        }
+                        else if (answer.StartsWith("歌名是：") && answer.Length >= 19)
+                        {
+                            actualAnswer = answer.Substring(4, 15);
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                        byte[] bytes1 = GetBytesFromString(_actualName);
+                        byte[] bytes2 = GetBytesFromString(actualAnswer);
+                        record.Score = 100.0 * GetCross(bytes1, bytes2) / GetMod(bytes1) / GetMod(bytes2);
+                        record.Answer = actualAnswer;
+                        _records.Add(record);
+                    }
+                }
+                _records = _records.OrderByDescending(a => a.Score).ToList();
             }
-            _records = _records.OrderByDescending(a => a.Score).ToList();
+            _loading = false;
+            StateHasChanged();
         }
-        _loading = false;
-        StateHasChanged();
     }
 }
+
+

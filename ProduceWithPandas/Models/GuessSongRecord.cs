0a852c6ccb48e40558b4f986b2dad5a9481c3cc9
Dong Bin <popmessiah@hotmail.com>
2021-09-22 21:01:25 +0800

    fix: fix namespace

--- ProduceWithPandas/Models/GuessSongRecord.cs
+++ ProduceWithPandas/Models/GuessSongRecord.cs
@@ -1,18 +1,22 @@
-﻿namespace ProduceWithPandas.Models;
+﻿using System;
 
-public class DoubanPostCache
+namespace ProduceWithPandas.Models
 {
-    public DateTime LastUpdateDate { get; set; }
-    public bool Initialized { get; set; } = false;
-    public List<DoubanReply> Replies { get; set; } = new List<DoubanReply>();
-}
+    public class DoubanPostCache
+    {
+        public DateTime LastUpdateDate { get; set; }
+        public bool Initialized { get; set; } = false;
+        public List<DoubanReply> Replies { get; set; } = new List<DoubanReply>();
+    }
 
-public class DoubanReply
-{
-    public int Id { get; set; }
-    public int UserId { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Answer { get; set; } = string.Empty;
-    public DateTime PublishTime { get; set; }
-    public double Score { get; set; }
+    public class DoubanReply
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Answer { get; set; } = string.Empty;
+        public DateTime PublishTime { get; set; }
+        public double Score { get; set; }
+    }
+
 }
